9d1648976a09028119822538389a82df1cf46fa3
lorinh <lorinh@21a73654-f31f-0410-a97e-e93c096abde9>
2008-01-14 19:57:43 +0000

    Now captures the version number
    git-svn-id: https://shelllogger.googlecode.com/svn/trunk@19 21a73654-f31f-0410-a97e-e93c096abde9

--- package/package.sh
+++ package/package.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -x
 cp ../readme.txt ../cli.py .
 rst2html.py readme.txt > readme.html
-zip shelllogger-r`svnversion` cli.py readme.txt readme.html
+svnversion > VERSION
+zip shelllogger-r`svnversion` cli.py readme.txt readme.html VERSION
 rm readme.txt readme.html cli.py

a24686db2c76120935ec1a195f4223367bca2d29
lorinh <lorinh@21a73654-f31f-0410-a97e-e93c096abde9>
2010-06-20 02:36:08 +0000

    Better command-line parsing
    git-svn-id: https://shelllogger.googlecode.com/svn/trunk@30 21a73654-f31f-0410-a97e-e93c096abde9

--- validate.py
+++ validate.py
@@ -7,11 +7,13 @@ It returns nothing if all XML files found validate.
 
 """
 import os
+from optparse import OptionParser
 from xml.parsers.expat import ExpatError
 import xml.etree.ElementTree as ET 
 
 
 def main(dirname):
+    print "Directory:", dirname
     for fname in os.listdir(dirname):
          if fname.endswith('.xml'):
             try:
@@ -22,4 +24,12 @@ def main(dirname):
                 print e
             
 if __name__ == '__main__':
+    usage = "usage: %prog [DIR]"
+    description = "Chceks that files with .xml extension in DIR are valid XML"
+    epilog = "If no directory is specified, will use DIR=~/.shelllogger"
+    parser = OptionParser(usage=usage, description=description, epilog=epilog)
+    (options, args) = parser.parse_args()
+    if len(args)>1:
+        parser.error("incorrect number of arguments")
+    
     main(dirname=os.path.expanduser('~/.shelllogger'))

9a94fa35593b1e46710973822d5339f20121b4f4
prakashkumar1988@gmail.com <prakashkumar1988@gmail.com@21a73654-f31f-0410-a97e-e93c096abde9>
2011-07-14 20:26:30 +0000

    easy install works :)
    git-svn-id: https://shelllogger.googlecode.com/svn/trunk@99 21a73654-f31f-0410-a97e-e93c096abde9

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ Setup file for installing shelllogger
 from setuptools import setup
 
 setup(
- name = "ShellLogger",
+ name = "shelllogger",
  version = "1.0",
  author = "Lorin Hochstein, Prakashkumar Thiagarajan",
  author_email = "lorinh@gmail.com, tprak@seas.upenn.edu",
  url = "http://code.google.com/p/shelllogger/",
- packages = ['source'],
+ packages = ["source"],
  scripts = ['shelllogger','sl-validate','sl-view'],
  description = "Logs shell commands, similar to Unix script program",
 )

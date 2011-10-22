02fde554a0417939886dbf16880ca9d725c622a5
Lorin Hochstein <lorin@isi.edu>
2011-10-22 12:51:59 -0400

    Removed old packaging script since we use Python packaging.

--- package/package.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -x
-mkdir shelllogger
-cp ../readme.txt ../shelllogger ../setup.py ../validate.py ../view.py shelllogger/
-rst2html.py shelllogger/readme.txt > shelllogger/readme.html
-svnversion > shelllogger/VERSION
-zip shelllogger-r`svnversion` shelllogger/*
-rm -rf shelllogger/
